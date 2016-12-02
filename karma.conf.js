5943272bc4c1dc368ccb8fbf86009550bfec798b
Igor Lamoš <igor@be.plus>
2016-12-02 14:19:37 +0100

    Added coverage reporting.

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,51 @@
+// retrieve args
+const argv = require('minimist')(process.argv.slice(2))
+const TRAVIS = process.env.TRAVIS
+// code coverage on by default on TRAVIS, or activated by flag --coverage
+const COVERAGE = argv.coverage === true || TRAVIS
+
+// the following env vars are used in webpack.config.js
+process.env.UNIT_TEST = true
+
 const webpackConfig = require('./webpack.config.test')
+const log = require('npmlog')
+log.level = 'silly'
+
+const plugins = [
+  'karma-webpack',
+  'karma-sinon',
+  'karma-mocha',
+  'karma-mocha-reporter',
+  'karma-sourcemap-loader',
+  'karma-chrome-launcher',
+  'karma-phantomjs-launcher'
+]
+
+const reporters = ['mocha']
+
+// default coverage reporter (we may want different reporters between local & CI)
+let coverageReporter = {
+  reporters: [
+    {type: 'lcov', dir: './build/reports/coverage'}
+  ]
+}
+
+if (COVERAGE) {
+  log.info('karma', 'COVERAGE mode enabled')
+  reporters.push('coverage')
+  plugins.push('karma-coverage')
+}
+if (COVERAGE && TRAVIS) {
+  log.info('karma', 'TRAVIS mode - will send coverage reports to coveralls.io')
+  reporters.push('coveralls')
+  plugins.push('karma-coveralls')
+  coverageReporter = { type: 'lcovonly', dir: './build/reports/coverage' }
+}
 
 module.exports = function karmaConfig (config) {
   config.set({
-    browsers: ['PhantomJS'],
-    singleRun: true,
-    frameworks: ['mocha'],
+    basePath: '',
+    frameworks: ['mocha', 'sinon'],
     files: [
       './node_modules/phantomjs-polyfill/bind-polyfill.js',
       './node_modules/babel-polyfill/browser.js',
@@ -12,18 +53,24 @@ module.exports = function karmaConfig (config) {
       './test/reactErrors.js' // ,
       // './src/**/*.js'
     ],
-    reporters: ['progress'], // coverage
+    browsers: ['PhantomJS'],
+    singleRun: true,
     preprocessors: {
-      'tests.webpack.js': ['webpack'] // ,
+      'tests.webpack.js': ['webpack', 'sourcemap'] // ,
       // 'src/**/*.js': ['coverage']
     },
     webpack: webpackConfig,
     webpackServer: {
       noInfo: true
-    } /* ,
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/'
-    } */
+    },
+
+    plugins: plugins,
+
+    coverageReporter: coverageReporter,
+
+    reporters: reporters,
+
+    colors: true,
+    logLevel: config.LOG_INFO
   })
 }
